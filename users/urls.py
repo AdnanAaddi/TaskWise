68353a733c55b228025f48c1131fa77b8eba3173
Adnan1 <m.adnan.sc@gmail.com>
2024-03-30 16:38:21 +0500

    converting views to classbased

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from users.views import *
+from .views import SignupView, LoginView,DashboardView, LogoutView
 urlpatterns = [
-    path('signup/', signup_view, name="signup"),
-    path('login/', login_view, name="login"),
-    path('dashboard/', dashboard_view, name="dashboard"),
-    path('logout/', logout_view, name="logout"),
+    path('signup/', SignupView.as_view(), name="signup"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('dashboard/', DashboardView.as_view(), name="dashboard"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render, redirect, HttpResponse
-from .models import ProjectUser
+from typing import Any
+from django.shortcuts import render, redirect
+from django.views import View
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.conf import settings
-# Create your views here.
-def signup_view(request):
-    if request.method == 'POST':
+from django.contrib.auth.mixins import LoginRequiredMixin
+from users.models import ProjectUser
+from projects.models import Project
+from django.views.generic import TemplateView, ListView, DetailView
+from projects.views import display_projects
+
+class SignupView(View):
+    def get(self, request):
+        return render(request, 'users/signup.html')
+
+    def post(self, request):
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
         password1 = request.POST.get('password1', '')
@@ -13,11 +20,9 @@ def signup_view(request):
         profile_picture = request.FILES.get('profile_picture', None)
 
         if name and email and password1 and password2:
-            # Check if email already exists
             if ProjectUser.objects.filter(email=email).exists():
-                print("User Alraedy Exists")
+                print("User Already Exists")
                 return render(request, 'users/signup.html')
-            
             
             user = ProjectUser.objects.create_user(name, email, password1)
             user.name = name
@@ -30,37 +35,43 @@ def signup_view(request):
             return redirect('/login/')
         else:
             print('Something went wrong')
-    else:
-        print('Just show the form!')
+            return render(request, 'users/signup.html')
 
-    return render(request, 'users/signup.html')
-            
-
-def login_view(request):
-    if request.method == 'POST':
-            email = request.POST['email']
-            password = request.POST['password']
-            username = ''
-            try:
-                username = ProjectUser.objects.get(email=email).username
-            except ProjectUser.DoesNotExist:
-                return HttpResponse("Invalid")
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                print("Logged in")
-                return redirect('dashboard')
-            else:
-                print("not logged in")
-            return redirect('signup')
-    else:
+class LoginView(View):
+    def get(self, request):
         return render(request, 'users/login.html')
 
-@login_required(login_url='/login/')
-def dashboard_view(request):
-    # The user is authenticated, render the dashboard
-    return render(request, "users/dashboard.html")
+    def post(self, request):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        username = ''
+        try:
+            username = ProjectUser.objects.get(email=email).username
+        except ProjectUser.DoesNotExist:
+            return redirect('invalid_login')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print("Logged in")
+            return redirect('dashboard')
+        else:
+            print("Not logged in")
+            return redirect('signup')
+        
+class DashboardView(LoginRequiredMixin, TemplateView):
+    print("Dashboard Called")
+    login_url = '/login/'
+    template_name = 'users/dashboard.html'
 
-def logout_view(request):
-    logout(request)
-    return redirect('login')
+    def get_context_data(self, **kwargs):
+        print("Context Method Called ")
+        context= super().get_context_data(**kwargs)
+        user_projects = display_projects(self.request.user)
+        print(self.request.user)
+        context['user_projects'] = user_projects
+        return context
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
